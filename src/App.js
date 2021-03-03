d1f4fc885c11cb805f937d7d36778252d6da0c76
Tushar Langer <tushar.langer@gmail.com>
2021-03-03 17:03:37 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone</h1>
+      <Header />
+      {/**Home */}
     </div>
   );
 }
