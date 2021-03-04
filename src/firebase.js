8921ebe9dbf171f9cb9616a2ec97fa62c5aa8ac9
Tushar Langer <tushar.langer@gmail.com>
2021-03-04 22:31:04 +0530

    user authentication done

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyBxW54jh72wjgLFs2M7WDKPAKhsZ4AngJo',
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: '1:857720720488:web:72e7d204909b35185affdd',
   measurementId: 'G-WJD0VE5E14',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
